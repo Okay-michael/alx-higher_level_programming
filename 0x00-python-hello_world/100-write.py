1b4a673b1d6b7c1dee702bce281a76fe61a0f777
Okay-michael <okomichaelakpu@gmail.com>
2024-01-02 14:57:25 +0100

    Write a Python script that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line.

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 import sys
-txt = "and that piece of art is useful - Dora Korpar, 2015-10-19, followed by\
-        a new line.\n"
-sys.stderr.write(txt)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 exit(1)
